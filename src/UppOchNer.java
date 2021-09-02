22dc934da0978ac23ccae17219c9a73035582a7b
helenahalldin <helena.halldin@hotmail.com>
2021-09-02 09:15:19 +0200

    Removes un nessesary code in UppOchNer

--- src/UppOchNer.java
+++ src/UppOchNer.java
@@ -3,60 +3,43 @@ import java.util.Scanner;
 
 public class UppOchNer {
     static Scanner scanner = new Scanner(System.in);
+    static final int maxNumberOfWords = 10;
+    static String[] words = new String[maxNumberOfWords];
 
     public static void run() {
         System.out.println("Välkommen till spelet \"Upp och Ner\"! ");
 
-        String[] words = getInput();
-        printUppAndDown(words);
+        getWordsFromUser();
+        printUppAndDown();
+        System.out.println(); //Avsluta med en tom rad.
+
     }
 
-    private static String[] getInput(){
-        int maxNumberOfWords = 10;
-        String[] words = initializeEmptyString(maxNumberOfWords);
+    //Get all words from the user, put in String[] and return it
+    private static void getWordsFromUser() {
+        Arrays.fill(words, "");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Skriv ett ord(Avsluta med \"END\", max antal ord är " + maxNumberOfWords + "): ");
-            String tempString = scanner.nextLine();
-            if (tempString.equals("END")) {
+        for (int i = 0; i < words.length; i++) {
+            String tempString = getInput();
+            if (tempString.equals("END"))
                 break;
-            } else
-                words[i] = tempString;
+            words[i] = tempString;
         }
 
-        return words;
     }
 
-    //Initialisera en array med tomma strängar.
-    private static String[] initializeEmptyString(int maxNumberOfWords) {
-        String[] words = new String[maxNumberOfWords];
-
-        for (int i = 0; i <words.length; i++)
-            words[i] = "";
-
-        return words;
+    //Get each input from the user
+    private static String getInput() {
+        System.out.println("Skriv ett ord(Avsluta med \"END\", max antal ord är " + maxNumberOfWords + "): ");
+        return scanner.nextLine();
     }
+
 
     //Skriv ut resultatet från spelet.
-    private static void printUppAndDown(String[] words) {
-        printStringArrayBackwards(words); //Skriv ut baklänges
-        System.out.println("END"); //Skriv ut END
-        printStringArray(words); //Skriv ut i "vanlig" ordning
-        System.out.println(); //Avsluta med en tom rad.
-    }
-
-    private static void printStringArray(String[] words) {
-        for (String word : words) {
-            if (!word.isEmpty())
-                System.out.println(word);
-        }
-    }
-
-    private static void printStringArrayBackwards(String[] words) {
+    private static void printUppAndDown() {
         for (int i = 0; i < words.length; i++) {
             if (!words[9 - i].isEmpty())
                 System.out.println(words[9 - i]);
         }
     }
-
 }
