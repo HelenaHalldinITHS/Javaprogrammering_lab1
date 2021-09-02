1a78d86289936de575cb087d7c8a551164954b89
helenahalldin <helena.halldin@hotmail.com>
2021-09-02 10:07:08 +0200

    "cleans" the code in StenSaxPase

--- src/StenSaxPase.java
+++ src/StenSaxPase.java
@@ -2,35 +2,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class StenSaxPase {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    
+    private static int playersScore = 0;
+    private static int computersScore = 0;
+    private static move playersChoice;
+    private static move computersChoice;
+    private final static int ROUNDS_TO_WINN = 3;
 
     enum move {
         STEN,
         SAX,
-        PÅSE
+        PASE
     }
 
     public static void run() {
-
-        int playersScore = 0;
-        int computersScore = 0;
-
-        while (playersScore < 3 && computersScore < 3) {
+        while (gameIsNotOver()) {
             printMenu();
-
-            move playersChoice = getPlayersChoice(); //spelarens val
-            move computersChoice = getComputersChoice(); //datorns val
-            choiceMessage(playersChoice, computersChoice);
-
-            if (!(playersChoice == computersChoice)) {
-                if (playerGetPoints(playersChoice, computersChoice))
-                    playersScore++;
-                else
-                    computersScore++;
-            }
-            printScore(playersScore, computersScore);
+            getPlayersChoice();
+            getComputersChoice();
+            printChoiceMessage();
+            giveWinnerPoint();
+            printScore();
         }
+        printWinnerMessage();
+    }
 
-        winnerMessage(playersScore, computersScore);
+    private static boolean gameIsNotOver() {
+        return playersScore < ROUNDS_TO_WINN && computersScore < ROUNDS_TO_WINN;
     }
 
     private static void printMenu() {
@@ -38,61 +38,69 @@ public class StenSaxPase {
         System.out.println("1. Sten");
         System.out.println("2. Sax");
         System.out.println("3. Påse");
-        System.out.println(""); //Tom rad
+        System.out.println(); 
+    }
+    
+    private static void getPlayersChoice() {
+        playersChoice = getChoiceFromInt(Integer.parseInt(scanner.nextLine()));
     }
 
-    private static move getPlayersChoice() {
-        Scanner scanner = new Scanner(System.in);
-        return getChoiceFromInt(Integer.parseInt(scanner.nextLine()));
-    }
-
-    private static move getComputersChoice() {
-        Random random = new Random();
-        return getChoiceFromInt(random.nextInt(3) + 1);
+    private static void getComputersChoice() {
+        computersChoice = getChoiceFromInt(random.nextInt(3) + 1);
     }
 
     private static move getChoiceFromInt(int choice) {
         return switch (choice) {
             case 1 -> move.STEN;
             case 2 -> move.SAX;
-            default -> move.PÅSE;
+            case 3 -> move.PASE;
+            default -> throw new IllegalStateException("Unexpected value: " + choice);
         };
     }
 
-    private static boolean playerGetPoints(move playersChoice, move computersChoice) {
-        boolean playerGetsPoint;
-
-        if (playersChoice == move.STEN)
-            playerGetsPoint = computersChoice != move.SAX;
-        else if (playersChoice == move.PÅSE)
-            playerGetsPoint = computersChoice != move.SAX;
-        else
-            playerGetsPoint = computersChoice != move.STEN;
-
-        return playerGetsPoint;
+    private static void printChoiceMessage() {
+        System.out.println("Du valde: " + playersChoice);
+        System.out.println("Datorn valde: " + computersChoice);
+        System.out.println();
     }
 
-    private static void winnerMessage(int playersScore, int computersScore) {
-        if (playersScore == 3) {
+    private static void giveWinnerPoint() {
+        if (!(playersChoice == computersChoice)) {
+            if (playerGetPoints())
+                playersScore++;
+            else
+                computersScore++;
+        }
+    }
+
+    private static boolean playerGetPoints() {
+        if (playersChoice == move.STEN)
+            return computersChoice != move.PASE;
+        else if (playersChoice == move.PASE)
+            return computersChoice != move.SAX;
+        else
+            return computersChoice != move.STEN;
+    }
+
+    private static void printScore() {
+        System.out.println("Antal vinster: ");
+        System.out.println("Spelare: " + playersScore);
+        System.out.println("Datorn: " + computersScore);
+        System.out.println();
+    }
+
+    private static void printWinnerMessage() {
+        if (playersScore == ROUNDS_TO_WINN) {
             System.out.println("Grattis DU vann!!");
         }
-        if (computersScore == 3) {
+        if (computersScore == ROUNDS_TO_WINN) {
             System.out.println("Datorn vann!");
         }
     }
 
-    private static void choiceMessage(move playersChoice, move computersChoice) {
-        System.out.println("Du valde: " + playersChoice);
-        System.out.println("Datorn valde: " + computersChoice);
-        System.out.println(""); //Tom rad
-    }
 
-    private static void printScore(int playersScore, int computersScore) {
-        System.out.println("Antal vinster: ");
-        System.out.println("Spelare: " + playersScore);
-        System.out.println("Datorn: " + computersScore);
-        System.out.println(""); //Tom rad
-    }
+
+
 
 
 }
